b2d7c05ca5daf8962c76c9b009b12e10c92b26bb
loicsay <loic.say@gmail.com>
2018-11-07 15:06:20 +0800

    style updated

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -7,18 +7,23 @@ class Board extends Component {
     return (
       <React.Fragment>
         <div className="container">
+          <h1>Board</h1>
           <div className="row">
             <div className="col-sm">
+              <h4>Teams</h4>
               <Card />
               <Card />
             </div>
             <div className="col-sm">
+              <h4>Priorities</h4>
               <Card />
             </div>
             <div className="col-sm">
+              <h4>Current Projects</h4>
               <Card />
             </div>
             <div className="col-sm">
+              <h4>Completed Projects</h4>
               <Card />
             </div>
           </div>
