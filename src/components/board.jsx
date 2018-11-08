1d894da4ca6f0ab5265aab903b90c72c015cad96
loicsay <loic.say@gmail.com>
2018-11-08 17:49:53 +0800

    code improvement and style modifications

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -4,11 +4,24 @@ import List from "./list";
 class Board extends Component {
   state = {
     cards: [
-      { id: 0, list: "Teams", content: "card 0" },
-      { id: 1, list: "Priorities", content: "card 1" },
-      { id: 2, list: "Current", content: "card 2" },
-      { id: 3, list: "Completed", content: "card 3" },
-      { id: 4, list: "Teams", content: "card 4" }
+      { id: 0, list: "Teams", content: "How to use this board" },
+      { id: 1, list: "Teams", content: "Product" },
+      { id: 2, list: "Teams", content: "Marketing" },
+      { id: 3, list: "Teams", content: "Sales" },
+      { id: 4, list: "Teams", content: "Support" },
+      { id: 5, list: "Teams", content: "People" },
+      { id: 6, list: "Teams", content: "IT" },
+      { id: 7, list: "Priorities", content: "Increase sales revenu by 30% in Q3" },
+      { id: 8, list: "Priorities", content: "Launch first international expansion" },
+      { id: 9, list: "Priorities", content: "Test new messaging for SMB market" },
+      { id: 10, list: "Current", content: "Website Redesign" },
+      { id: 11, list: "Current", content: "Ship iOS app" },
+      { id: 12, list: "Current", content: "Analytics Data" },
+      { id: 13, list: "Current", content: "Increase conversion rate by 20% by Q3" },
+      { id: 14, list: "Current", content: "Develop Engineering Blog" },
+      { id: 15, list: "Current", content: "Brand Guidelines" },
+      { id: 16, list: "Completed", content: "Social Media Campaign" },
+      { id: 17, list: "Completed", content: "Update Help Documentation" }
     ],
     lists: [
       { id: 0, name: "Teams" },
@@ -24,14 +37,13 @@ class Board extends Component {
   };
 
   handleEdit = (content, cardId) => {
-    const cards = this.state.cards;
+    const cards = [...this.state.cards];
     for (let id in cards) {
       if (Number(id) === cardId) {
         cards[id].content = content;
       }
     }
     this.setState(cards); 
-    console.log(this.state);
   };
 
   render() {
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,31 +1,27 @@
 import React, { Component } from "react";
 
 class Card extends Component {
-
-  handleOnBlur = (e) => {
+  handleOnBlur = e => {
     this.props.onEdit(e.target.value, this.props.id);
-  }
+  };
 
   render() {
     return (
-      <>
         <div className="card">
           <div className="card-body">
-            <input
-              type="text"
-              className="form-control"
-              onBlur={this.handleOnBlur}
-              defaultValue={this.props.content}
-            />
             <button
               className="btn-danger btn-sm"
               onClick={() => this.props.onDelete(this.props.id)}
-            >
-              x
-            </button>
+            />
+            <div className="form-group" onBlur={this.handleOnBlur}>
+              <textarea
+                className="form-control"
+                rows="2"
+                defaultValue={this.props.content}
+              />
+            </div>
           </div>
         </div>
-      </>
     );
   }
 }
