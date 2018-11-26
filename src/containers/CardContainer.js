54da78ffde4e70abf1b9c93e564264b2b57b6c7a
loicsay <loic.say@gmail.com>
2018-11-26 16:39:29 +0800

    redux

--- src/containers/CardContainer.js
+++ src/containers/CardContainer.js
@@ -1,10 +1,21 @@
 import { connect } from "react-redux";
 import { addCard } from "../actions/cardActions";
+import { editCard } from "../actions/cardActions";
+import { deleteCard } from "../actions/cardActions";
+import { onDragCardStart } from "../actions/cardActions";
+import { onDropCard } from "../actions/cardActions";
+import { onDragCardEnd } from "../actions/cardActions";
+
 import Card from "../components/Card";
 
 const mapDispatchToProps = dispatch => {
   return {
-    addCard: id => dispatch(addCard(id))
+    addCard: id => dispatch(addCard(id)),
+    editCard: (id, newCardName) => dispatch(editCard(id, newCardName)),
+    deleteCard: id => dispatch(deleteCard(id)),
+    onDragCardStart: (id, target) => dispatch(onDragCardStart(id, target)),
+    onDropCard: (id, target) => dispatch(onDropCard(id, target)),
+    onDragCardEnd: (id, target) => dispatch(onDragCardEnd(id, target))
   };
 };
 
